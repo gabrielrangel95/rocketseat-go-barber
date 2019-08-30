6277e65f7840792905676b82bd772cb4123098b3
Gabriel Avila Rangel <gabriel.avila.rangel@gmail.com>
2019-08-30 08:12:30 -0300

    fix: docker location

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
   host: '127.0.0.1',
-  username: 'docker',
+  username: 'postgres',
   password: 'docker',
-  database: 'gonodemodulo2',
+  database: 'gonode',
   operatorAliases: false,
   define: {
     timestamps: true,
